f73b6177fd7275c36fba2c63a1063b5b2a83ec8c
Masayuki Usui <usui-masayuki394@g.ecc.u-tokyo.ac.jp>
2023-05-14 22:10:14 +0900

    Add login and listing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+
+export default function App() {
+  const [login, setLogin] = useState(0);
+  const [taskList, setTaskList] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+
+    fetch('http://localhost:8000/users/', {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+
+    fetch('http://localhost:8000/users/', {
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'include'
+    })
+    .then(response => response.json())
+    .then(data => setTaskList(data));
+
+    setLogin(1);
+  }
+
+  const taskListItems = taskList.map(task => <li key={task.task_id}>{task.title}</li>);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {login ? (
+        <ul>{taskListItems}</ul>
+      ) : (
+        <form method="post" onSubmit={handleSubmit}>
+          <label>
+            Username: <input type="text" name="username" />
+          </label>
+          <label>
+            Password: <input type="password" name="password" />
+          </label>
+          <button type="reset">Reset</button>
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </div>
-  );
+  )
 }
-
-export default App;
